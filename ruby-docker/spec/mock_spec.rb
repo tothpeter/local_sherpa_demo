0af75df5aa6c3056fcc33122145f56e1fe7f29bd
Peter Toth <tothpeter08@gmail.com>
2024-12-01 12:13:18 +0700

    Add RuboCop to the Ruby project for linting

--- ruby-docker/spec/mock_spec.rb
+++ ruby-docker/spec/mock_spec.rb
@@ -2,7 +2,7 @@
 
 describe "Mock test" do
   51.times do
-    it "does something" do
+    it "does something" do # rubocop:disable RSpec/NoExpectationExample
       sleep(rand(0.01..0.02))
     end
   end
